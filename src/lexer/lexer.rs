125b6e6f63893d7d7670f4d9c50dc00cf4054d67
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2020-08-21 22:59:33 -0300

    Added == != ! - <> true false if else return / *

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -24,6 +24,10 @@ impl<'a> Iterator for Lexer<'a> {
             0 => None,
             _ => {
                 if is_special(remainder[0] as char) {
+                    if can_be_to_digit(remainder[0] as char) && (remainder[1] as char) == '=' {
+                        self.position += 2;
+                        return Some(String::from_utf8(vec![remainder[0], remainder[1]]).unwrap())
+                    }
                     self.position += 1;
                     Some(String::from_utf8(vec![remainder[0]]).unwrap())
                 } else {
@@ -41,9 +45,18 @@ fn is_special(ch: char) -> bool {
     special_chars.contains(&ch)
 }
 
+fn can_be_to_digit(ch: char) -> bool {
+    let can_be_to_digit_chars: [char; 2] = ['=', '!'];
+    can_be_to_digit_chars.contains(&ch)
+}
+
+fn convert_to_tokens(input: &str) -> Vec<Token> {
+    Lexer::new(input).filter(|x| x != " " && x != "\n").map(|x| get_token(&x)).collect::<Vec<_>>()
+}
+
 #[test]
 fn it_works_iterator() {
-    let input = "let five = 5;
+    let input = "let five == 5;
 let ten = 10;
 
 let add = fn(x, y) {
@@ -52,8 +65,8 @@ let add = fn(x, y) {
 
 let result = add(five, ten);
 ";
-    let letters: Vec<_> = Lexer::new(input).filter(|x| x != " " && x != "\n").collect();
-    let test = vec!["let", "five", "=", "5", ";", "let",
+    let letters = Lexer::new(input).filter(|x| x != " " && x != "\n").collect::<Vec<_>>();
+    let test = vec!["let", "five", "==", "5", ";", "let",
                     "ten", "=", "10", ";", "let", "add", "=",
                     "fn", "(", "x", ",", "y", ")", "{", "x",
                     "+", "y", ";", "}", ";", "let", "result",
@@ -80,6 +93,36 @@ let result = add(five, ten);
                     Token::RParen, Token::LBrace, Token::Ident(String::from("x")), Token::Plus, Token::Ident(String::from("y")), Token::Semicolon, Token::RBrace, Token::Semicolon,
                     Token::Let, Token::Ident(String::from("result")), Token::Assign, Token::Ident(String::from("add")),  Token::LParen, Token::Ident(String::from("five")),
                     Token::Comma, Token::Ident(String::from("ten")), Token::RParen, Token::Semicolon
+    ];
+    assert_eq!(letters, test);
+}
+
+#[test]
+fn it_works_with_more() {
+    let input = "let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+if (5 < 10) {
+return true;
+} else {
+return false;
+}
+10 == 10;
+10 != 9;
+";
+    let letters: Vec<_> = Lexer::new(input).filter(|x| x != " " && x != "\n").map(|x| get_token(&x)).collect();
+    let test = vec![Token::Let, Token::Ident(String::from("five")), Token::Assign, Token::Int(5), Token::Semicolon, Token::Let, Token::Ident(String::from("ten")), Token::Assign, Token::Int(10), Token::Semicolon,
+                    Token::Let, Token::Ident(String::from("add")), Token::Assign, Token::Function, Token::LParen, Token::Ident(String::from("x")), Token::Comma, Token::Ident(String::from("y")), Token::RParen,
+                    Token::LBrace, Token::Ident(String::from("x")), Token::Plus, Token::Ident(String::from("y")), Token::Semicolon, Token::RBrace, Token::Semicolon, Token::Let, Token::Ident(String::from("result")),
+                    Token::Assign, Token::Ident(String::from("add")), Token::LParen,
+                    Token::Ident(String::from("five")), Token::Comma, Token::Ident(String::from("ten")), Token::RParen, Token::Semicolon, Token::Eof, Token::Semicolon, Token::Int(5), Token::LT, Token::Int(10), Token::GT, Token::Int(5),
+                    Token::Semicolon, Token::If, Token::LParen, Token::Int(5), Token::LT, Token::Int(10), Token::RParen, Token::LBrace, Token::Return, Token::True, Token::Semicolon, Token::RBrace, Token::Else, Token::LBrace, Token::Return,
+                    Token::False, Token::Semicolon, Token::RBrace, Token::Int(10), Token::Eq, Token::Int(10), Token::Semicolon, Token::Int(10), Token::Bang, Token::Assign, Token::Int(9), Token::Semicolon
     ];
     assert_eq!(letters, test);
 }
--- src/token/token.rs
+++ src/token/token.rs
@@ -8,6 +8,12 @@ pub enum Token {
     //Operators
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Eq,
+    NotEq,
     //Delimiters
     Comma,
     Semicolon,
@@ -15,26 +21,57 @@ pub enum Token {
     RParen,
     LBrace,
     RBrace,
+    LT,
+    GT,
     //Keyword
     Function,
     Let,
+    True,
+    False,
+    If,
+    Else,
+    Return
 }
 
 pub fn get_token(keyword: &str) -> Token {
     match keyword {
         "fn" => Token::Function,
         "let" => Token::Let,
+        "true" => Token::True,
+        "false" => Token::False,
+        "if" => Token::If,
+        "else" => Token::Else,
+        "return" => Token::Return,
+        "==" => Token::Eq,
+        "!=" => Token::NotEq,
         "=" => Token::Assign,
         "+" => Token::Plus,
+        "-" => Token::Minus,
+        "!" => Token::Bang,
+        "*" => Token::Asterisk,
+        "/" => Token::Slash,
         "," => Token::Comma,
         ";" => Token::Semicolon,
         "(" => Token::LParen,
         ")" => Token::RParen,
         "{" => Token::LBrace,
         "}" => Token::RBrace,
+        "<" => Token::LT,
+        ">" => Token::GT,
         &_ => match keyword.parse::<i64>() {
             Ok(t) => Token::Int(t),
-            Err(_) => Token::Ident(keyword.to_string())
+            Err(_) => match ident_is_invalid(keyword) {
+                false => Token::Ident(keyword.to_string()),
+                true => Token::Eof
+            }
         }
     }
+}
+
+fn ident_is_invalid(ident: &str) -> bool {
+   ident.as_bytes().iter().any(|x| !is_letter(*x as char))
+}
+
+fn is_letter(ch: char) -> bool {
+    'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' || ch == '!' || ch == '?'
 }
